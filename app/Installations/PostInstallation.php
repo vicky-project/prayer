84abb618e1ea31872b2d6ad31ac082d1ed49ed76
vicky <pratamavr@gmail.com>
2026-03-14 23:01:39 +0800

    updates

---
+++ app/Installations/PostInstallation.php
@@ -0,0 +1,26 @@
+<?php
+namespace Modules\Prayer\Installations;
+
+use Nwidart\Modules\Facades\Module;
+use Illuminate\Support\Facades\Artisan;
+
+class PostInstallation
+{
+  public function handle(string $moduleName) {
+    try {
+      $modules = array_merge(["users", "telegram"], [$moduleName]);
+      foreach ($modules as $modulename) {
+        $module = Module::find($modulename);
+        $module->enable();
+      }
+
+    } catch (\Exception $e) {
+      logger()->error(
+        "Failed to run post installation of object task module: " .
+        $e->getMessage(),
+      );
+
+      throw $e;
+    }
+  }
+}
