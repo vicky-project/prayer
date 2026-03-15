d88911678ea4a452fdc682c996c62495efb0bc37
vicky <pratamavr@gmail.com>
2026-03-15 23:27:34 +0800

    updates

--- app/Http/Controllers/PrayerController.php
+++ app/Http/Controllers/PrayerController.php
@@ -39,7 +39,7 @@ class PrayerController extends Controller
 
       if (!$res->successful()) {
         \Log::error("Failed to get data prayer.", [
-          "body" => $res->json()
+          "body" => $res->object()->error
         ]);
         return response()->json(["success" => false, "message" => "Gagal mengambil data shalat"]);
       }
