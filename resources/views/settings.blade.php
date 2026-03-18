79b7974b5ec4a59e5d6c85658c368d9d512812af
vicky <pratamavr@gmail.com>
2026-03-19 06:31:50 +0800

    updates

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -151,7 +151,16 @@
         autoLocationBtn.disabled = false;
         },
         (error) => {
+        if (error.code === error.PERMISSION_DENIED) {
+        // Bisa karena user menolak, ATAU karena insecure origin
+        if (window.location.protocol !== 'https:' && window.location.hostname !== 'localhost') {
+        locationStatus.innerText = 'Akses lokasi memerlukan koneksi HTTPS. Gunakan input manual.';
+        } else {
+        locationStatus.innerText = 'Izin lokasi ditolak. Gunakan input manual.';
+        }
+        } else {
         locationStatus.innerText = "Gagal mendapatkan lokasi: " + error.message;
+        }
         autoLocationBtn.disabled = false;
         },
         { enableHighAccuracy: true, timeout: 10000 }
