ffc9bec8ac03380f675c3afe8f4209f67584ad5d
vicky <pratamavr@gmail.com>
2026-03-23 14:30:52 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\Prayer\Http\Controllers\PrayerController;
 
 Route::prefix("apps")
-->name("apps.")->middleware(["web", "telegram.miniapp"])
+->name("apps.")->middleware(["web"])
 ->group(function () {
   Route::get("prayer", [PrayerController::class, "index"])->name("prayer");
   Route::get("prayer/settings", [PrayerController::class, "settings"])->name("prayer.settings");
